6de3b2708765b32a6ea1930f6f25bb25d758479c
Macavity <300609+Macavity@users.noreply.github.com>
2024-10-12 21:27:28 +0200

    fix: re-add support for html but restrict it to template content
    - closes #5

--- src/components/ValueTypes/RelationValue.svelte
+++ src/components/ValueTypes/RelationValue.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-    import type { IAVCellValue } from "siyuan";
-    import AttributeViewRollup from "@/components/AttributeViewRollup.svelte";
+  import type { IAVCellValue } from "siyuan";
+  import AttributeViewRollup from "./AttributeViewRollup.svelte";
 
-    export let value: IAVCellValue;
-    const lastId = value.relation.contents[value.relation.contents.length - 1].id
+  export let value: IAVCellValue;
+  const lastId = value.relation.contents[value.relation.contents.length - 1].id;
 </script>
 
 {#if value?.relation?.contents}
-    {#each value.relation.contents as item}
-        {#if item}
-            <AttributeViewRollup value={item} />
-            {#if item.id !== lastId}
-                ,&nbsp;
-            {/if}
-        {/if}
-    {/each}
+  {#each value.relation.contents as item}
+    {#if item}
+      <AttributeViewRollup value={item} />
+      {#if item.id !== lastId}
+        ,&nbsp;
+      {/if}
+    {/if}
+  {/each}
 {/if}
