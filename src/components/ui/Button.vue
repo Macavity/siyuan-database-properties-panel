ce31c671ecd37f5fe4716ea069dd2b647391163c
Alexander Pape <300609+Macavity@users.noreply.github.com>
2026-03-28 15:04:15 +0100

    ♻️ refactor: migrate Button component to Vue

---
+++ src/components/ui/Button.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Icon from "@/components/ui/Icon.vue";
+import type { SiYuanIcon } from "@/types/SiyuanIcon";
+
+const props = withDefaults(
+  defineProps<{
+    label?: string | null;
+    icon?: SiYuanIcon | null;
+    isFocused?: boolean;
+    tooltip?: string | null;
+    class?: string | null;
+  }>(),
+  {
+    label: null,
+    icon: null,
+    isFocused: false,
+    tooltip: null,
+    class: null,
+  }
+);
+
+const emit = defineEmits<{
+  click: [event: Event];
+}>();
+
+function handleClick(event: Event) {
+  emit("click", event);
+}
+</script>
+
+<template>
+  <button
+    :class="[
+      'block__icon block__icon--show',
+      props.class,
+      {
+        'b3-tooltips__w': props.tooltip !== null,
+        'b3-tooltips': props.tooltip !== null,
+        'item--focus': props.isFocused,
+      },
+    ]"
+    :aria-label="props.tooltip"
+    @click="handleClick"
+    @keydown="handleClick"
+  >
+    <Icon v-if="props.icon" :icon="props.icon" />
+    <span v-if="props.label" class="item__text">{{ props.label }}</span>
+  </button>
+</template>
