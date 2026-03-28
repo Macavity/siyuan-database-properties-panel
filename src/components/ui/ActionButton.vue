dad3b5eb40b00de4df951d8dbc167363bfef98e0
Alexander Pape <300609+Macavity@users.noreply.github.com>
2026-03-28 15:04:20 +0100

    ♻️ refactor: migrate ActionButton component to Vue

---
+++ src/components/ui/ActionButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Icon from "@/components/ui/Icon.vue";
+import type { SiYuanIcon } from "@/types/SiyuanIcon";
+
+const props = withDefaults(
+  defineProps<{
+    label?: string | null;
+    icon?: SiYuanIcon | null;
+    class?: string | null;
+  }>(),
+  {
+    label: null,
+    icon: null,
+    class: null,
+  }
+);
+
+const emit = defineEmits<{
+  click: [event: MouseEvent];
+}>();
+</script>
+
+<template>
+  <button
+    :class="['b3-button b3-button--cancel', props.class]"
+    @click="emit('click', $event)"
+  >
+    <Icon v-if="props.icon" :icon="props.icon" />
+    <template v-if="props.label">{{ props.label }}</template>
+  </button>
+</template>
