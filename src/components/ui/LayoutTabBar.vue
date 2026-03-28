6fb895901dc6fc59ee9d18ffc0cd2b04c4a06e9c
Alexander Pape <300609+Macavity@users.noreply.github.com>
2026-03-28 15:05:15 +0100

    ♻️ refactor: migrate LayoutTabBar component to Vue

---
+++ src/components/ui/LayoutTabBar.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import Icon from "@/components/ui/Icon.vue";
+import type { SiYuanIcon } from "@/types/SiyuanIcon";
+
+interface Tab {
+  key: string;
+  name: string;
+  dataType?: string;
+  icon?: SiYuanIcon;
+}
+
+defineProps<{
+  tabs: Tab[];
+  focus: string;
+}>();
+
+const emit = defineEmits<{
+  click: [key: string];
+}>();
+
+function activateTab(key: string) {
+  emit("click", key);
+}
+</script>
+
+<template>
+  <div class="fn__flex layout-tab-bar-wrapper">
+    <div class="layout-tab-bar fn__flex fn__flex-1">
+      <div
+        v-for="(tab, index) in tabs"
+        :key="tab.key"
+        :class="['item item--full', { 'item--focus': tab.key === focus }]"
+        :data-type="tab.dataType"
+        role="button"
+        :tabindex="index"
+        @click="activateTab(tab.key)"
+        @keydown="activateTab(tab.key)"
+      >
+        <span class="fn__flex-1"></span>
+        <Icon v-if="tab.icon" class="block__logoicon" :icon="tab.icon" />
+        <span class="item__text">{{ tab.name }}</span>
+        <span class="fn__flex-1"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout-tab-bar-wrapper {
+  padding: 0 8px;
+}
+</style>
